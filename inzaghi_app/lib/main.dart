b658f62019757d9aceb062e61bbff44913cebda5
inzaghidev <izzumiposhaf29@gmail.com>
2024-03-04 00:39:09 +0700

    Add Files

--- inzaghi_app/lib/main.dart
+++ inzaghi_app/lib/main.dart
@@ -1,7 +1,9 @@
-// main.dart
 import 'package:flutter/material.dart';
-import 'pages/AboutPage.dart';
-import 'pages/AppsPage.dart';
+import 'package:inzaghi_app/pages/about_page.dart';
+import 'package:inzaghi_app/pages/apps_page.dart';
+import 'package:inzaghi_app/pages/settings_page.dart';
+import 'package:inzaghi_app/screens/home_page.dart';
+import 'package:inzaghi_app/widgets/navbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,63 +15,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => MainPage(),
+        '/': (context) => HomePage(), // Use Home widget as the main route
         '/about': (context) => AboutPage(),
         '/apps': (context) => AppsPage(),
-        //'/inzaghis-group': (context) => InzaghisGroup(),
+        '/settings': (context) => SettingsPage(),
+        // Add other routes here
       },
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Inzaghi's App"),
-      ),
-      body: SingleChildScrollView(
-        // Wrap the Column with SingleChildScrollView
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 30),
-              Text(
-                "Welcome to Inzaghi's App!",
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "This App Platform is still Under Construction!",
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 40),
-              Image.asset(
-                'assets/images/inzaghis-app-by-inzaghis-group-corp.png',
-                width: 350,
-                height: 224,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/about');
-                },
-                child: Text('About'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/apps');
-                },
-                child: Text('Apps'),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
